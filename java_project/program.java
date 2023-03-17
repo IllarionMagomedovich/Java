a01a22e7e71d3912d2d702c0713399fb378507da
IllarionMagomedovich <vadim.evenko98@mail.ru>
2023-03-17 23:31:36 +0300

    ok

---
+++ java_project/program.java
@@ -0,0 +1,13 @@
+package java_project;
+
+public class program {
+    /**
+     * Innerprogram
+     */
+    public class Innerprogram {
+        public static void main(String[] args) {
+            System.out.println("world");
+        }
+        
+    }
+}
